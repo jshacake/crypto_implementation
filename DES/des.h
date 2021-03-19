738c266cda20f4609273f90a5fdd9f617a883bc9
jshacake <jshacake@gmail.com>
2021-03-19 23:14:59 +0900

    Delete des.h

--- DES/des.h
+++
@@ -1,2 +0,0 @@
-void enc(unsigned char p[8], unsigned char k[8], unsigned char c[8]);	//	result is stored in c(ciphertext)
-void dec(unsigned char c[8], unsigned char k[8], unsigned char p[8]);	//	result is stored in p(plaintext)
